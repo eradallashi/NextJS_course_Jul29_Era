da9c63fc5a42554374d978cfc099c5f82243e360
Era <era.dallashi@yahoo.com>
2024-09-06 13:19:05 +0200

    es6

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
 
-function withdraw(person, amount) {
+/*function withdraw(person, amount) {
 
 
     if (amount > person.balance) {
@@ -20,3 +20,23 @@ const person = {
 
 withdraw(person, 1000); 
 withdraw(person, 400);  
+
+*/
+
+const withdraw = ({ balance }, amount) => {
+    if (amount > balance) {
+        console.log("Withdrawal cannot be processed due to insufficient funds.");
+    } else {
+        balance -= amount;
+        console.log(`Withdrawal successful. New balance: $${balance}`);
+    }
+};
+
+const person = {
+    name: "Era Dallashi",
+    balance: 700
+};
+
+withdraw(person, 1000);
+withdraw(person, 400);
+
